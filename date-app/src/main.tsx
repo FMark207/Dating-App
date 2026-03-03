17ccb3ff1b83407844d95f4de0ddb61c37793fbe
bmiluu <banmilan07@gmail.com>
2026-03-03 14:18:00 +0100

    added ProfileEditor.tsx

--- date-app/src/main.tsx
+++ date-app/src/main.tsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ProfileEditor from './components/ProfileEditor.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ProfileEditor/>
 )
