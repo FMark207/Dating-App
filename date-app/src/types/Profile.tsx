ea74cce29459c4b7f0a405010f1cd2ab5e057a46
bmiluu <banmilan07@gmail.com>
2026-03-05 09:47:39 +0100

    added functionality

---
+++ date-app/src/types/Profile.tsx
@@ -0,0 +1,10 @@
+export interface Profile{
+  name: string;
+  age: number;
+  image: string;
+  location: string;
+  distance: string;
+  bio: string;
+  interests: string[];
+  fun_facts:string[];
+}
